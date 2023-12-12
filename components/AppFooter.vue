59e73526ef0c841a0a9231cd727ec0a6f87b0210
都木　聡一郎 <146801529+Torimaruorimaru573@users.noreply.github.com>
2023-12-12 05:47:21 +0000

    1

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -3,7 +3,7 @@
     <div class="w-full my-10 text-center select-none text-gray-500" style="letter-spacing: 20px">°°°</div>
     <ul class="flex flex-col mb-3 justify-center space-y-2 sm:space-y-0 sm:space-x-5 sm:flex-row">
       <li>
-        <NuxtLink to="/あいうえお" class="transition-colors duration-300 rounded px-2 py-1 hover:bg-gray-400">About</NuxtLink>
+        <NuxtLink to="/about" class="transition-colors duration-300 rounded px-2 py-1 hover:bg-gray-400">About</NuxtLink>
       </li>
       <li>
         <NuxtLink to="/profile" class="transition-colors duration-300 rounded px-2 py-1 hover:bg-gray-400">Profile</NuxtLink>
