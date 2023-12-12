c3b0becf05c739d102df09f37a1845a9439f91b7
都木　聡一郎 <146801529+Torimaruorimaru573@users.noreply.github.com>
2023-12-12 05:23:43 +0000

    1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <AppHeader />
-    <AppAside />
-    <slot />
-    <AppFooter />
+  <div class="py-16 mx-auto max-w-2xl">
+    <!-- components/AppHeader.vue -->
+    <app-header />
+    <!-- パスに一致するpagesファイルが呼ばれる -->
+    <Nuxt />
+    <!-- components/AppFooter.vue -->
+    <app-footer />
   </div>
 </template>
