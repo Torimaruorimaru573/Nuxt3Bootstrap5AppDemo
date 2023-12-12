dbe79b4956f032d9713567a3f6e01d38e9cbf67a
都木　聡一郎 <146801529+Torimaruorimaru573@users.noreply.github.com>
2023-12-12 05:38:56 +0000

    1

--- pages/article/_slug.vue
+++ pages/article/_slug.vue
@@ -1,18 +1,24 @@
-<template>
-  <main>
-    <h1 class="text-3xl text-center">{{ article.title }}</h1>
-    <nuxt-content :document="article"/>
-  </main>
-</template>
-
-<script>
-export default {
-  async asyncData({ $content, params }) {
-    const article = await $content(params.slug).fetch()
-
-    return {
-      article
-    }
-  }
+<style>
+.nuxt-content * {
+  margin-top: 15px;
+  margin-bottom: 15px;
 }
-</script>
+.nuxt-content h1 {
+  background-color: #000000A0;
+  color: #fff;
+  padding: 10px 20px;
+}
+.nuxt-content h2 {
+  border-left: 5px solid #000000A0;
+  padding-left: 5px;
+}
+.nuxt-content h3 {
+  border-bottom: 2px solid #000000A0;
+}
+.nuxt-content ul {
+  list-style: disc;
+}
+.nuxt-content a {
+  color: blue;
+}
+</style>
