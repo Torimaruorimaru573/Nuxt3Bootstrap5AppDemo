a48fe54e59781ee373b288e818f59701520f7863
都木　聡一郎 <146801529+Torimaruorimaru573@users.noreply.github.com>
2023-12-19 05:37:46 +0000

    1

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      ...
+    </nav>
+    <nuxt/>
+    <hr>
+    <!-- Footer -->
+    <footer>
+      ...
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  a {
+    color: #212529;
+    transition: all 0.2s;
+  }
+
+  #mainNav {
+    position: absolute;
+    border-bottom: 1px solid #e9ecef;
+    background-color: white;
+    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  footer {
+    padding: 50px 0 65px;
+    text-align: center;
+  }
+</style>
