e2b2a54c7ce3475b0130ac777a708bffa1337433
都木　聡一郎 <146801529+Torimaruorimaru573@users.noreply.github.com>
2024-01-30 05:13:14 +0000

    a

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- your template code here -->
+  </template>
+  
+  <script>
+  export default {
+    // your script code here
+  }
+  </script>
+  
+  <style>
+    /* your style code here */
+  </style>
+  
